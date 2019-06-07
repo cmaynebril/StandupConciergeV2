cbc2b05798c2d136a972e3eceae36ac89cdecf11
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-07 10:32:41 +0800

    include questionsQuestion

--- StandupConciergeV2/Models/StandupConciergeContext.cs
+++ StandupConciergeV2/Models/StandupConciergeContext.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StandupConciergeV2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace StandupConciergeV2.Models
 {
-    public class StandupConciergeContext : DbContext
+    public class StandupConciergeContext : IdentityDbContext<User, UserRole, int>
     {
         public StandupConciergeContext(DbContextOptions<StandupConciergeContext> options)
          : base(options)
@@ -15,6 +17,7 @@ namespace StandupConciergeV2.Models
         public DbSet<Schedule> Schedules { get; set; }
         public DbSet<Question> Questions{ get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<StandupConciergeV2.Models.QuestionsQuestion> QuestionsQuestion { get; set; }
     }
     
 }
