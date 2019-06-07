48c89992cf5aec3cc7d22682a9c593521d003e7a
Christopher May Nebril <christopher.nebril@dbaadvisory.com>
2019-06-07 10:33:20 +0800

    User Identity

--- StandupConciergeV2/Models/User.cs
+++ StandupConciergeV2/Models/User.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace StandupConciergeV2.Models
 {
-    public class User
+    public class User : IdentityUser <int>
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string EmailAdd { get; set; }
+       
     }
+
 }
